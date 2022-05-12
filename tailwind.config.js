9a46cefa3945db9ee4c8c71c5010f64d454dc22e
ariful892 <ariful00892@gmail.com>
2022-05-12 16:42:52 +0600

    navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary: "#f6d860",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
